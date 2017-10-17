63723bf8296244f2dedf6746456eb80b0104b497
iraislevy95 <irais.levy@alumnos.udg.mx>
2017-10-16 23:04:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/iraisyishar/transporta2/MainWindow.java
@@ -0,0 +1,47 @@
+package com.example.iraisyishar.transporta2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainWindow extends AppCompatActivity {
+
+    Button btnBuscaRuta, btnRecordatorio, btnReporte;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_window);
+
+        btnBuscaRuta = (Button)findViewById(R.id.btnBuscaRuta);
+        btnRecordatorio = (Button)findViewById(R.id.btnRecordatorio);
+        btnReporte = (Button)findViewById(R.id.btnReporte);
+
+        btnRecordatorio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainWindow.this, Recordatorio.class);
+                startActivity(intent);
+            }
+        });
+
+        btnBuscaRuta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainWindow.this, BuscarRuta.class);
+                startActivity(intent);
+            }
+        });
+
+        btnReporte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainWindow.this, Reporte.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/iraisyishar/transporta2/Recordatorio.java
@@ -0,0 +1,58 @@
+package com.example.iraisyishar.transporta2;
+
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class Recordatorio extends AppCompatActivity {
+
+    Spinner spRuta;
+    Button btnHora;
+    TextView tvHora;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recordatorio);
+
+        spRuta = (Spinner)findViewById(R.id.spRuta);
+        btnHora = (Button)findViewById(R.id.btnHora);
+        tvHora = (TextView)findViewById(R.id.tvHora);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Rutas, android.R.layout.simple_spinner_item);
+        spRuta.setAdapter(adapter);
+
+        btnHora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get Current time
+                final Calendar c = Calendar.getInstance();
+                final int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(Recordatorio.this,
+                        new TimePickerDialog.OnTimeSetListener(){
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                                if(minute<10 && hourOfDay>10) tvHora.setText(hourOfDay + ":0" + minute);
+                                else if(hourOfDay<10 && minute>10)tvHora.setText( "0" + hourOfDay + ":" + minute);
+                                else if(hourOfDay<10 && minute<10)tvHora.setText( "0" + hourOfDay + ":0" + minute);
+                                else tvHora.setText(hourOfDay + ":" + minute);
+
+                            }
+                        }, hour, minute, false);
+                timePickerDialog.show();
+            }
+        });
+    }
+}
